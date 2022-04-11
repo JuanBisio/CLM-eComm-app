4c55f21fac32790a495f4c25f7e195efdb39c8ad
JuanBisio <71271495+JuanBisio@users.noreply.github.com>
2022-04-10 21:59:11 -0300

    Primer commit, subida de archivos

---
+++ CLM-eComm-admin/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+            Hola Mundo
+        </h1>
+        <p>Esta es mi primera pagaina web con react</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
